5846c2900d3600a11197f32e53cdfd4d40f2550b
alxrw <alxr.wagner@gmail.com>
2023-04-13 14:53:20 +0700

    Initial commit

---
+++ src/main/java/com/example/awsomepastebin/AwsomePastebinApplication.java
@@ -0,0 +1,13 @@
+package com.example.awsomepastebin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsomePastebinApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AwsomePastebinApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/awsomepastebin/AwsomePastebinApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.awsomepastebin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwsomePastebinApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
